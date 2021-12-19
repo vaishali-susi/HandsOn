f77db3954cf15cda7a6325295cad558ca7168e1e
vaishali-susi <vaishalisharma23111998@gmail.com>
2021-12-19 16:21:05 +0530

    third commit

--- test/practice.js
+++ test/practice.js
@@ -1,15 +1,46 @@
-// fetch('https://meme-api.herokuapp.com/gimme').then((response)=>{
-//     return response;
+// let r = fetch('obj.json');
+// r.then((res)=>{
+// return res;
 // }).then((data)=>{
-//     fn(data);
+//     console.log(data);
 // })
-// function fn(data){
-//     console.log(data.json());
+// function a(x){
+//     b(x);
+// }
+// function b(x){
+// console.log(x);
+// }
+// a(925);
+
+
+// let a = fetch('obj.js').then((response)=>{
+// return response;
+// })
+// let b = a.then((data)=>{
+//     console.log(data) ;
+// })
+
+// let r = fetch("https://jsonplaceholder.typicode.com/posts").then((response)=>{
+//  return response;
+// })
+// let s = r.then(function(data){
+//     return data.json();   
+// });
+// s.then((e)=>{
+//     return e;
+// })
+
+// let n = 23;
+
+// while(n!=0){
+//     let dig = Math.floor(n%10);
+//     console.log(dig);
+//     Math.floor(n=n/10);
 // }
 
-let a = fetch('obj.js').then((response)=>{
-return response;
-})
-let b = a.then((data)=>{
-    console.log(data.json()) ;
-})
+let a= 10;
+a=20;
+let b = 20;
+b=10;
+console.log(a);
+console.log(b);
